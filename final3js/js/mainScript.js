05f2470fd095ae65abea4e2cbb1fe2ad37e9c79e
not-a-genius <romagiuppo@gmail.com>
2019-08-20 17:08:44 +0200

    up

--- final3js/js/mainScript.js
+++ final3js/js/mainScript.js
@@ -332,34 +332,13 @@ function animate(now) {
 			zombieAnimated.walkingAnimate(myDelta,walkSpeed)
 		}
 	
-
+				// Detect collisions.
+		if ( collisions.length > 0 ) {
+			detectCollisions();
+		}
 	}
 
 
-
-	// if (keyboard[87]) { // W key
-	// 	camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
-	// 	camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
-	// }
-	// if (keyboard[83]) { // S key
-	// 	camera.position.x += Math.sin(camera.rotation.y) * player.speed;
-	// 	camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
-	// }
-	// if (keyboard[65]) { // A key
-	// 	camera.position.x += Math.sin(camera.rotation.y + Math.PI / 2) * player.speed;
-	// 	camera.position.z += -Math.cos(camera.rotation.y + Math.PI / 2) * player.speed;
-	// }
-	// if (keyboard[68]) { // D key
-	// 	camera.position.x += Math.sin(camera.rotation.y - Math.PI / 2) * player.speed;
-	// 	camera.position.z += -Math.cos(camera.rotation.y - Math.PI / 2) * player.speed;
-	// }
-
-	// if (keyboard[37]) { // left arrow key
-	// 	camera.rotation.y -= player.turnSpeed;
-	// }
-	// if (keyboard[39]) { // right arrow key
-	// 	camera.rotation.y += player.turnSpeed;
-	// }
 
 
 
--- final3js/js/temp.js
+++ final3js/js/temp.js
@@ -187,3 +187,37 @@ function importZombie() {
 	// 	scene.add( object );
 
 	// } );
+
+
+
+
+
+
+/*
+Old controls
+*/
+
+
+	// if (keyboard[87]) { // W key
+	// 	camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
+	// 	camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
+	// }
+	// if (keyboard[83]) { // S key
+	// 	camera.position.x += Math.sin(camera.rotation.y) * player.speed;
+	// 	camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
+	// }
+	// if (keyboard[65]) { // A key
+	// 	camera.position.x += Math.sin(camera.rotation.y + Math.PI / 2) * player.speed;
+	// 	camera.position.z += -Math.cos(camera.rotation.y + Math.PI / 2) * player.speed;
+	// }
+	// if (keyboard[68]) { // D key
+	// 	camera.position.x += Math.sin(camera.rotation.y - Math.PI / 2) * player.speed;
+	// 	camera.position.z += -Math.cos(camera.rotation.y - Math.PI / 2) * player.speed;
+	// }
+
+	// if (keyboard[37]) { // left arrow key
+	// 	camera.rotation.y -= player.turnSpeed;
+	// }
+	// if (keyboard[39]) { // right arrow key
+	// 	camera.rotation.y += player.turnSpeed;
+	// }
